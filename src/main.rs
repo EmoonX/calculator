164b781693cfd3dcad11c4a4842ea8feba4b05a1
EmoonX <gustavo.chicato@usp.br>
2022-03-21 19:44:44 -0300

    Pick three args (one by one) and print them

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 use std::env::args;
 
 fn main() {
-    let args = args();
-    println!("{:?}", args);
+    // Get program arguments
+    let mut args = args();
+
+    // Pick first arg after binary path (which is skipped)
+    let first = args.nth(1).unwrap();
+
+    // Pick following args (iterator is resumed from last access)
+    let operator = args.nth(0).unwrap();
+    let second = args.nth(0).unwrap();
+
+    println!("{} {} {}", first, operator, second);
 }
